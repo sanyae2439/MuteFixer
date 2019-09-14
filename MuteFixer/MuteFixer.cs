2d77d2ddb0d8eb5b86558f366208ef06744a93f0
hatsunemiku24 <hatsunemiku_39@yahoo.co.jp>
2019-09-14 21:28:00 +0900

    init

---
+++ MuteFixer/MuteFixer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using MEC;
+using Smod2;
+using Smod2.Attributes;
+using Smod2.EventHandlers;
+using Smod2.Events;
+
+namespace MuteFixer
+{
+    [PluginDetails(
+    author = "sanyae2439",
+    name = "MuteFixer",
+    description = "Fix persistant mute",
+    id = "sanyae2439.MuteFixer",
+    version = "1.0",
+    SmodMajor = 3,
+    SmodMinor = 5,
+    SmodRevision = 0
+    )]
+    public class MuteFixer : Plugin
+    {
+        public override void OnDisable()
+        {
+            this.Info("MuteFixer Disabled...");
+        }
+
+        public override void OnEnable()
+        {
+            this.Info("MuteFixer Enabled!");
+        }
+
+        public override void Register()
+        {
+            this.AddEventHandlers(new JoinHandler());
+        }
+    }
+
+    public class JoinHandler : IEventHandlerPlayerJoin
+    {
+        public void OnPlayerJoin(PlayerJoinEvent ev)
+        {
+            Timing.RunCoroutine(this._DelayedForceSyncMute(), Segment.FixedUpdate);
+        }
+
+        public IEnumerator<float> _DelayedForceSyncMute()
+        {
+            yield return Timing.WaitForSeconds(0.25f);
+            foreach(var ccm in UnityEngine.Object.FindObjectsOfType<CharacterClassManager>())
+            {
+                if(ccm.Muted) ccm.SetDirtyBit(1u);
+            }
+            yield break;
+        }
+    }
+}
